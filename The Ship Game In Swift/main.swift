f280227174da94405d5f69dddf8cf79d0cf7f06d
Harry Chen <harrychen55@gmail.com>
2015-11-19 20:08:02 +0800

    Initial Commit

---
+++ The Ship Game In Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  The Ship Game In Swift
+//
+//  Created by Harry on 15/11/19.
+//  Copyright © 2015年 Harry Chen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
